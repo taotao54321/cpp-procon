8eb50c9df55c4001edda40dc1c530174fc0da060
taotao54321 <taotao54321@gmail.com>
2019-03-24 17:05:24 +0900

    テスト追加: 1次元累積和

---
+++ test/cum1/solve.cpp
@@ -0,0 +1,29 @@
+#include "procon.hpp"
+#include "cum.hpp"
+
+void solve(i64 N) {
+    Cum1<i64> cum(N);
+    REP(i, N) {
+        i64 a; RD(a);
+        cum.add(i, a);
+    }
+    cum.build();
+
+    i64 ans = -INF;
+    FOR(i, 0, N) FOR(j, i, N) {
+        i64 k = j - i + 1;
+        chmax(ans, cum.query(i, k));
+    }
+
+    PRINTLN(ans);
+}
+
+signed main() {
+    for(;;) {
+        i64 N; RD(N);
+        if(N == 0) break;
+        solve(N);
+    }
+
+    EXIT();
+}
