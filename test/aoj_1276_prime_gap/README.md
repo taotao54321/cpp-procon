d52cb8f4d188220975454a860e4e7553970c335f
taotao54321 <taotao54321@gmail.com>
2019-03-27 18:22:29 +0900

    テスト追加: AOJ 1276 - Prime Gap
    is_prime_table() テスト用

---
+++ test/aoj_1276_prime_gap/README.md
@@ -0,0 +1,5 @@
+# AOJ 1276 - Prime Gap
+
+<https://onlinejudge.u-aizu.ac.jp/problems/1276>
+
+`is_prime_table()` テスト用
---
+++ test/aoj_1276_prime_gap/solve.cpp
@@ -0,0 +1,26 @@
+#include "procon.hpp"
+#include "num.hpp"
+
+signed main() {
+    decltype(auto) prime = is_prime_table<1299710>();
+
+    for(;;) {
+        i64 K; RD(K);
+        if(K == 0) break;
+
+        if(prime[K]) {
+            PRINTLN(0);
+            continue;
+        }
+
+        i64 l = K;
+        i64 r = K;
+        while(!prime[l]) --l;
+        while(!prime[r]) ++r;
+        i64 ans = r - l;
+
+        PRINTLN(ans);
+    }
+
+    EXIT();
+}
