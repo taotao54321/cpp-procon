306c9469c223db5f57577901ea71dd86b88e98f0
taotao54321 <taotao54321@gmail.com>
2019-03-28 05:10:17 +0900

    grid.hpp のテストを追加

---
+++ test/aoj_1160_how_many_islands/README.md
@@ -0,0 +1,5 @@
+# AOJ 1160 - How Many Islands?
+
+<http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1160&lang=jp>
+
+grid.hpp テスト用
---
+++ test/aoj_1160_how_many_islands/solve.cpp
@@ -0,0 +1,38 @@
+#include "procon.hpp"
+#include "container.hpp"
+#include "grid.hpp"
+
+void solve(i64 H, i64 W) {
+    Grid2<i64> grid(H,W);
+    REP(y, H) REP(x, W) {
+        RD(grid.at(y,x));
+    }
+
+    auto dfs = FIX([&grid](auto self, const Index2& idx) -> void {
+        grid[idx] = 0;
+        for(const auto& to : grid.neighbor8(idx)) {
+            if(grid[to] == 0) continue;
+            self(to);
+        }
+    });
+
+    i64 ans = 0;
+    REP(y, H) REP(x, W) {
+        if(grid.at(y,x) == 1) {
+            ++ans;
+            dfs(Index2(y,x));
+        }
+    }
+
+    PRINTLN(ans);
+}
+
+signed main() {
+    for(;;) {
+        i64 W,H; RD(W); RD(H);
+        if(H == 0 && W == 0) break;
+        solve(H, W);
+    }
+
+    EXIT();
+}
