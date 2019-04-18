271dc718954a44f4d513b889f4b06bfebe84b62b
taotao54321 <taotao54321@gmail.com>
2019-04-18 16:53:11 +0900

    string.hpp
    とりあえず CHARS_LOWER など

---
+++ include/string.hpp
@@ -0,0 +1,9 @@
+// string {{{
+
+const string CHARS_DIGIT("0123456789");
+const string CHARS_LOWER("abcdefghijklmnopqrstuvwxyz");
+const string CHARS_UPPER("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+const string CHARS_ALPHA("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+const string CHARS_ALNUM("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+
+// }}}
