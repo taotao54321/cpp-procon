7effb687b017707832df697b0d7d9fb41acf6eb3
taotao54321 <taotao54321@gmail.com>
2019-06-26 11:21:48 +0900

    最小限のテンプレを追加
    なるべく自力でコーディングする練習用

---
+++ tmpl/solve-mini.cpp
@@ -0,0 +1,95 @@
+// header {{{
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define CPP_STR(x) CPP_STR_I(x)
+#define CPP_CAT(x,y) CPP_CAT_I(x,y)
+#define CPP_STR_I(args...) #args
+#define CPP_CAT_I(x,y) x ## y
+
+#define ASSERT(expr...) assert((expr))
+
+using i8  = int8_t;
+using u8  = uint8_t;
+using i16 = int16_t;
+using u16 = uint16_t;
+using i32 = int32_t;
+using u32 = uint32_t;
+using i64 = int64_t;
+using u64 = uint64_t;
+
+using f32 = float;
+using f64 = double;
+using f80 = __float80;
+// }}}
+
+constexpr i64 INF = 1'010'000'000'000'000'017LL;
+
+constexpr i64 MOD = 1'000'000'007LL;
+
+constexpr f64 EPS = 1e-12;
+
+constexpr f64 PI = 3.14159265358979323846;
+
+// util {{{
+#define FOR(i, start, end) for(i64 i = (start), CPP_CAT(i,xxxx_end)=(end); i < CPP_CAT(i,xxxx_end); ++i)
+#define REP(i, n) FOR(i, 0, n)
+
+#define ALL(f,c,...) (([&](decltype((c)) cccc) { return (f)(std::begin(cccc), std::end(cccc), ## __VA_ARGS__); })(c))
+
+template<typename C>
+i64 SIZE(const C& c) { return static_cast<i64>(c.size()); }
+
+template<typename T, size_t N>
+i64 SIZE(const T (&)[N]) { return static_cast<i64>(N); }
+
+template<typename T, typename U, typename Comp=less<>>
+bool chmax(T& xmax, const U& x, Comp comp={}) {
+    if(comp(xmax, x)) {
+        xmax = x;
+        return true;
+    }
+    return false;
+}
+
+template<typename T, typename U, typename Comp=less<>>
+bool chmin(T& xmin, const U& x, Comp comp={}) {
+    if(comp(x, xmin)) {
+        xmin = x;
+        return true;
+    }
+    return false;
+}
+// }}}
+
+// init {{{
+struct ProconInit {
+    static constexpr int IOS_PREC = 15;
+    static constexpr bool AUTOFLUSH = false;
+
+    ProconInit() {
+        cin.tie(nullptr);
+        ios::sync_with_stdio(false);
+        cout << fixed << setprecision(IOS_PREC);
+        if(AUTOFLUSH)
+            cout << unitbuf;
+    }
+} PROCON_INIT;
+// }}}
+
+//--------------------------------------------------------------------
+
+void solve() {
+    
+
+    // cout << ans << "\n";
+}
+
+signed main() {
+    
+
+    solve();
+
+    return 0;
+}
