e5b660f88dfbd3705d1cf30a278b5f1a97d3115b
taotao54321 <taotao54321@gmail.com>
2020-01-18 15:00:19 +0900

    z_function

---
+++ include/z_function.hpp
@@ -0,0 +1,24 @@
+// z_function {{{
+
+template<typename RandomIt>
+vector<i64> z_function(RandomIt first, RandomIt last) {
+    i64 n = last - first;
+    vector<i64> z(n, 0);
+
+    i64 l = 0;
+    i64 r = 0;
+    FOR(i, 1, n) {
+        if(i < r)
+            z[i] = min(r-i, z[i-l]);
+
+        while(i+z[i] < n && first[i+z[i]] == first[z[i]])
+            ++z[i];
+
+        if(chmax(r, i+z[i]))
+            l = i;
+    }
+
+    return z;
+}
+
+// }}}
