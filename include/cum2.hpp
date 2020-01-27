bbdd3205558a7d73ed1c41758df1c8565261347f
taotao54321 <taotao54321@gmail.com>
2020-01-27 19:27:11 +0900

    cum2
    アーベル群を扱える2次元累積和。演算が普通の和の場合は cum2_default() で
    楽に作れる。
    
    1次元累積和はクラス化するほどでもないと判断。
    
    従来の cum は attic へ。

---
+++ include/cum2.hpp
@@ -0,0 +1,70 @@
+// cum2 {{{
+
+template<class Abel, class Op, class Inv>
+struct Cum2 {
+    Op op_;
+    Inv inv_;
+    Abel unity_;
+
+    vector<vector<Abel>> v_;
+    bool built_{false};
+
+    Cum2(Op&& op, Inv&& inv, const Abel& unity, i64 h, i64 w) :
+        op_(forward<Op>(op)), inv_(forward<Inv>(inv)), unity_(unity),
+        v_(h+1,vector<Abel>(w+1,unity_))
+    {}
+
+    i64 h() const { return SIZE(v_)-1; }
+    i64 w() const { return SIZE(v_[0])-1; }
+
+    const Abel& at(i64 y, i64 x) const {
+#ifdef PROCON_LOCAL
+        ASSERT(!built_);
+#endif
+        return v_[y+1][x+1];
+    }
+
+    Abel& at(i64 y, i64 x) {
+#ifdef PROCON_LOCAL
+        ASSERT(!built_);
+#endif
+        return v_[y+1][x+1];
+    }
+
+    void build() {
+#ifdef PROCON_LOCAL
+        ASSERT(!built_);
+#endif
+        FOR(y, 1, h()+1) {
+            partial_sum(begin(v_[y])+1, end(v_[y]), begin(v_[y])+1, op_);
+            FOR(x, 1, w()+1) {
+                v_[y][x] = op_(v_[y-1][x], v_[y][x]);
+            }
+        }
+        built_ = true;
+    }
+
+    Abel query(i64 y1, i64 x1, i64 y2, i64 x2) {
+#ifdef PROCON_LOCAL
+        ASSERT(built_);
+        ASSERT(y1 <= y2 && x1 <= x2);
+#endif
+        Abel res = v_[y2][x2];
+        res = op_(res, inv_(v_[y1][x2]));
+        res = op_(res, inv_(v_[y2][x1]));
+        res = op_(res, v_[y1][x1]);
+        return res;
+    }
+};
+
+template<class Abel, class Op, class Inv>
+auto cum2_make(Op&& op, Inv&& inv, const Abel& unity, i64 h, i64 w) {
+    return Cum2<Abel,Op,Inv>(forward<Op>(op), forward<Inv>(inv), unity, h, w);
+}
+
+template<class T>
+auto cum2_default(i64 h, i64 w) {
+    return cum2_make<T>(plus<>{}, negate<>{}, T{}, h, w);
+}
+
+// }}}
