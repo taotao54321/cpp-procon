0034821d3bd89ad0dcc5fd4f0ab6f134b924dccc
taotao54321 <taotao54321@gmail.com>
2020-02-01 23:51:44 +0900

    verify: AOJ CGL_2_[AB]

---
+++ verify/aoj/CGL_2_A/solve.test.cpp
@@ -0,0 +1,66 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/2/CGL_2_A"
+//#define ERROR "1e-6"
+
+// header {{{
+#include "../../../include/stdc++.hpp"
+using namespace std;
+
+using i8  = int8_t;
+using u8  = uint8_t;
+using i16 = int16_t;
+using u16 = uint16_t;
+using i32 = int32_t;
+using u32 = uint32_t;
+using i64 = int64_t;
+using u64 = uint64_t;
+#ifdef __SIZEOF_INT128__
+using i128 = __int128;
+using u128 = unsigned __int128;
+#endif
+
+using f32 = float;
+using f64 = double;
+using f80 = long double;
+
+template<class T> constexpr T PROCON_INF();
+// }}}
+
+using Real = f80;
+
+template<> constexpr i64  PROCON_INF<i64>()  { return INT64_C(1'010'000'000'000'000'017); }
+template<> constexpr Real PROCON_INF<Real>() { return Real(1e100L); }
+
+constexpr i64 MOD = INT64_C(1'000'000'007);
+//constexpr i64 MOD = INT64_C(998'244'353);
+
+constexpr Real EPS = Real(1e-10L);
+
+constexpr int  COUT_PREC      = 15;
+constexpr bool COUT_AUTOFLUSH = false;
+
+#include "../../../include/procon.hpp"
+#include "../../../include/geo.hpp"
+
+//--------------------------------------------------------------------
+
+void solve() {
+    i64 Q = RD();
+    LOOP(Q) {
+        auto PS = RD_VEC<Vec>(4);
+        auto v1 = PS[1] - PS[0];
+        auto v2 = PS[3] - PS[2];
+
+        if(geo_is_parallel(v1, v2))
+            PRINTLN(2);
+        else if(geo_is_orthogonal(v1, v2))
+            PRINTLN(1);
+        else
+            PRINTLN(0);
+    }
+}
+
+signed main() {
+    solve();
+
+    EXIT();
+}
---
+++ verify/aoj/CGL_2_B/solve.test.cpp
@@ -0,0 +1,62 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/2/CGL_2_B"
+//#define ERROR "1e-6"
+
+// header {{{
+#include "../../../include/stdc++.hpp"
+using namespace std;
+
+using i8  = int8_t;
+using u8  = uint8_t;
+using i16 = int16_t;
+using u16 = uint16_t;
+using i32 = int32_t;
+using u32 = uint32_t;
+using i64 = int64_t;
+using u64 = uint64_t;
+#ifdef __SIZEOF_INT128__
+using i128 = __int128;
+using u128 = unsigned __int128;
+#endif
+
+using f32 = float;
+using f64 = double;
+using f80 = long double;
+
+template<class T> constexpr T PROCON_INF();
+// }}}
+
+using Real = f80;
+
+template<> constexpr i64  PROCON_INF<i64>()  { return INT64_C(1'010'000'000'000'000'017); }
+template<> constexpr Real PROCON_INF<Real>() { return Real(1e100L); }
+
+constexpr i64 MOD = INT64_C(1'000'000'007);
+//constexpr i64 MOD = INT64_C(998'244'353);
+
+constexpr Real EPS = Real(1e-10L);
+
+constexpr int  COUT_PREC      = 15;
+constexpr bool COUT_AUTOFLUSH = false;
+
+#include "../../../include/procon.hpp"
+#include "../../../include/geo.hpp"
+
+//--------------------------------------------------------------------
+
+void solve() {
+    i64 Q = RD();
+    LOOP(Q) {
+        auto PS = RD_VEC<Vec>(4);
+        Segment s1(PS[0], PS[1]);
+        Segment s2(PS[2], PS[3]);
+
+        bool ans = geo_intersects(s1, s2);
+        PRINTLN(ans ? 1 : 0);
+    }
+}
+
+signed main() {
+    solve();
+
+    EXIT();
+}
