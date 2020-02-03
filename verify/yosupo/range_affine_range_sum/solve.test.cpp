bd53dd9a353a58c49133a4bf6003b0d792788e79
taotao54321 <taotao54321@gmail.com>
2020-02-03 23:39:49 +0900

    verify: yosupo range_affine_range_sum

---
+++ verify/yosupo/range_affine_range_sum/solve.test.cpp
@@ -0,0 +1,97 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/range_affine_range_sum"
+//#define ERROR "1e-6"
+
+#define ASSERT_LV 2
+// header {{{
+#include "../../../include/stdc++.hpp"
+using namespace std;
+
+using i8  = int8_t;
+using u8  = uint8_t;
+using i16 = int16_t;
+using u16 = uint16_t;
+using i32 = int32_t;
+using u32 = uint32_t;
+using i64 = int64_t;
+using u64 = uint64_t;
+#ifdef __SIZEOF_INT128__
+using i128 = __int128;
+using u128 = unsigned __int128;
+#endif
+
+using f32 = float;
+using f64 = double;
+using f80 = long double;
+
+template<class T> constexpr T PROCON_INF();
+// }}}
+
+using Real = f80;
+
+template<> constexpr i64  PROCON_INF<i64>()  { return INT64_C(1'010'000'000'000'000'017); }
+template<> constexpr Real PROCON_INF<Real>() { return Real(1e100L); }
+
+//constexpr i64 MOD = INT64_C(1'000'000'007);
+constexpr i64 MOD = INT64_C(998'244'353);
+
+constexpr Real EPS = Real(1e-10L);
+
+constexpr int  COUT_PREC      = 15;
+constexpr bool COUT_AUTOFLUSH = false;
+
+#include "../../../include/procon.hpp"
+#include "../../../include/segtree.hpp"
+
+//--------------------------------------------------------------------
+
+void solve() {
+    i64 N = RD();
+    i64 Q = RD();
+
+    using Monoid = pair<i64,ModInt>;
+    using Action = pair<ModInt,ModInt>;
+
+    auto A = vec_reserve<Monoid>(N);
+    LOOP(N) {
+        i64 a = RD();
+        A.emplace_back(1, a);
+    }
+
+    auto seg = segtree_lazy_range<Monoid,Action>(
+        [](const Monoid& ml, const Monoid& mr) -> Monoid { return {ml.first+mr.first, ml.second+mr.second}; },
+        [](const Monoid& m, const Action& a) -> Monoid {
+            auto [b,c] = a;
+            return {m.first, b*m.second + c*m.first};
+        },
+        [](const auto& al, const auto& ar) -> Action {
+            auto [b1,c1] = al;
+            auto [b2,c2] = ar;
+            return {b1*b2, b2*c1 + c2};
+        },
+        make_pair(1,0),
+        make_pair(1,0),
+        begin(A), end(A)
+    );
+    LOOP(Q) {
+        i64 cmd = RD();
+        if(cmd == 0) {
+            i64 l = RD();
+            i64 r = RD();
+            Action a = RD<Action>();
+            seg.act(l, r, a);
+        }
+        else if(cmd == 1) {
+            i64 l = RD();
+            i64 r = RD();
+            auto ans = seg.query(l,r).second;
+            PRINTLN(ans);
+        }
+        else { ASSERT(false); }
+    }
+}
+
+signed main() {
+    solve();
+
+    EXIT();
+}
