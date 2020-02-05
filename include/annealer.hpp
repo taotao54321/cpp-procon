42d5b6233a693658d8b53dc913a9a27cdb1ec7d1
taotao54321 <taotao54321@gmail.com>
2020-02-05 23:22:05 +0900

    annealer
    焼きなまし法。温度スケジュールと遷移確率関数を渡せる。
    annealer_default() は温度線形変化、遷移確率 exp(dscore/temp)。

---
+++ include/annealer.hpp
@@ -0,0 +1,49 @@
+// annealer {{{
+
+template<class TempF, class ProbF>
+class Annealer {
+public:
+    Annealer(TempF&& ft, ProbF&& fp, Real temp_start, Real temp_end) :
+        ft_(forward<TempF>(ft)), fp_(forward<ProbF>(fp)),
+        temp_start_(temp_start), temp_end_(temp_end), temp_(temp_start_)
+    {}
+
+    Real temp() const { return temp_; }
+
+    Real prob(Real dscore) const {
+        return dscore > 0 ? 1.0 : fp_(temp_,dscore);
+    }
+
+    void update(Real r) {
+        temp_ = ft_(temp_start_, temp_end_, r);
+    }
+
+    bool accepts(Real dscore) {
+        if(dscore > 0) return true;
+        Real p = fp_(temp_, dscore);
+        return dist_(PROCON_URBG()) < p;
+    }
+
+private:
+    TempF ft_;
+    ProbF fp_;
+    Real temp_start_;
+    Real temp_end_;
+    Real temp_;
+    uniform_real_distribution<Real> dist_{};
+};
+
+template<class TempF, class ProbF>
+auto annealer_make(TempF&& ft, ProbF&& fp, Real temp_start, Real temp_end) {
+    return Annealer<TempF,ProbF>(forward<TempF>(ft), forward<ProbF>(fp), temp_start, temp_end);
+}
+
+auto annealer_default(Real temp_start, Real temp_end) {
+    return annealer_make(
+        [](Real ts, Real te, Real r) { return ts + r*(te-ts); },
+        [](Real temp, Real dscore) { return exp(dscore/temp); },
+        temp_start, temp_end
+    );
+}
+
+// }}}
